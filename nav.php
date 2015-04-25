78c9c2ffd7bd52b2832abb1bb3c5cd75e9abebc0
akw28888 <akw2312@gmail.com>
2015-04-25 17:20:45 +0800

    nav.php: SSNODE TAIWAN changes

--- nav.php
+++ nav.php
@@ -1,10 +1,8 @@
 <div class="header">
     <ul class="nav nav-pills pull-right" role="tablist">
         <li role="presentation"><a href="index.php">Home</a></li>
-        <li role="presentation"><a href="https://plus.google.com/u/0/communities/105993338677212487777">Forum</a></li>
-        <li role="presentation"><a href="faq.php">FAQ</a></li>
-        <li role="presentation"><a href="user/register.php">Reg</a></li>
-        <li role="presentation"><a href="user/index.php">UserCenter</a></li>
+        <li role="presentation"><a href="user/register.php">Register</a></li>
+        <li role="presentation"><a href="user/index.php">Control Panel</a></li>
     </ul>
     <h3 class="text-muted"><?php echo $site_name; ?></h3>
 </div>
