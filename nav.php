9fb1037c286ec4b89da7a795a52122f9d59638b3
akw28888 <akw2312@gmail.com>
2015-04-26 18:41:53 +0800

    nav.php: fix nav

--- nav.php
+++ nav.php
@@ -1,15 +1,8 @@
 <div class="header">
     <ul class="nav nav-pills pull-right" role="tablist">
-<<<<<<< HEAD
         <li role="presentation"><a href="index.php">Home</a></li>
         <li role="presentation"><a href="user/register.php">Register</a></li>
         <li role="presentation"><a href="user/index.php">Control Panel</a></li>
-=======
-        <li role="presentation"><a href="index.php">Home</a></li> 
-        <li role="presentation"><a href="faq.php">FAQ</a></li>
-        <li role="presentation"><a href="user/register.php">Reg</a></li>
-        <li role="presentation"><a href="user/index.php">UserCenter</a></li>
->>>>>>> 6e44e4b... update nav
     </ul>
     <h3 class="text-muted"><?php echo $site_name; ?></h3>
 </div>
