24d31f9aa82a96e46ce21fea65e0ec3b2c984911
akw28888 <akw2312@gmail.com>
2015-05-01 01:21:41 +0800

    footer.php: remove sponsor

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <div class="footer">
     Copyright 2014-2015 Lop Legend Group.
     <br>Powered by <a href="https://github.com/orvice/ss-panel">ss-panel</a> 
-    <br>Sponsor: <a href="http://www.ipfog.com/">云雾互联</a>
     <br>If you need invite codes, please e-mail to akw@gfw.red
 </div>
