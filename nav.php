0f764823d4bc4044bf01a5886b217c31b6b0bc41
kaneawk <kaneawk@gmail.com>
2015-05-02 14:50:13 +0800

    fix some bad strings for O.C.D
    Conflicts:
    	footer.php
    	index.php
    	nav.php
    	user/tos.php

--- nav.php
+++ nav.php
@@ -1,8 +1,15 @@
 <div class="header">
     <ul class="nav nav-pills pull-right" role="tablist">
+<<<<<<< HEAD
         <li role="presentation"><a href="index.php">Home</a></li>
         <li role="presentation"><a href="user/register.php">Register</a></li>
         <li role="presentation"><a href="user/index.php">Control Panel</a></li>
+=======
+        <li role="presentation"><a href="index.php">主页</a></li>
+        <li role="presentation"><a href="faq.php">FAQ</a></li>
+        <li role="presentation"><a href="user/register.php">注册</a></li>
+        <li role="presentation"><a href="user/index.php">登录</a></li>
+>>>>>>> b70c1f1... fix some bad strings for O.C.D
     </ul>
     <h3 class="text-muted"><?php echo $site_name; ?></h3>
 </div>
