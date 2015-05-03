1eb7993f1484d0c44b7caf59167ea9e57e96d3ba
orvice <orvice@gmail.com>
2015-05-03 13:50:47 +0800

    update nav
    Conflicts:
    	nav.php

--- nav.php
+++ nav.php
@@ -3,6 +3,7 @@
         <li role="presentation"><a href="index.php">Home</a></li>
         <li role="presentation"><a href="user/register.php">Register</a></li>
         <li role="presentation"><a href="user/index.php">Control Panel</a></li>
+        <li role="presentation"><a href="code.php">Invite Code</a></li>
     </ul>
     <h3 class="text-muted"><?php echo $site_name; ?></h3>
 </div>
