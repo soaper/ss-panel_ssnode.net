2fc101caf008ff2b99cd93d242285f8346db3e2f
akw28888 <akw2312@gmail.com>
2015-05-05 21:21:55 +0800

    update for website.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
     Copyright 2014-2015 Lop Legend Group.
     <br>Powered by <a href="https://github.com/orvice/ss-panel">ss-panel</a> 
-    <br>If you need invite codes, please e-mail to akw@gfw.red
+    <br>ssnode (ssnode.com ssnode.me) 正式改收費制度 R.I.P
 </div>
