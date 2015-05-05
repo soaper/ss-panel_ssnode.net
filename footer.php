ce6267497868ab7b5846157e8b5e93d17265aa01
akw28888 <akw2312@gmail.com>
2015-05-05 21:34:13 +0800

    Fix website because of cherry-pick. Revert some from commit "fix some bad strings for O.C.D"

--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 <div class="footer">
-<<<<<<< HEAD
     Copyright 2014-2015 Lop Legend Group.
     <br>Powered by <a href="https://github.com/orvice/ss-panel">ss-panel</a> 
     <br>ssnode (ssnode.com ssnode.me) 正式改收費制度 R.I.P
-=======
-    <p>&copy; <?php echo $site_name."  ".date('Y'); ?>  Powered by <a href="https://github.com/orvice/ss-panel">ss-panel</a> <?php echo $version; ?>
-        耗时：<?php
-        $Runtime->Stop();
-        echo $Runtime->SpendTime()."ms";
-        ?>
-    </p>
->>>>>>> b70c1f1... fix some bad strings for O.C.D
 </div>
--- index.php
+++ index.php
@@ -12,30 +12,11 @@ include_once 'header.php';
 	<p> 邀請碼先到先得: https://ssnode.net/code.php</p>
         <p><a class="btn btn-lg btn-success" href="user/register.php" role="button">立即注册</a></p>
     </div>
-<<<<<<< HEAD
-    <?php
-=======
-
-    <div class="row marketing">
-        <div class="col-lg-6">
-            <a href="https://play.google.com/store/apps/details?id=com.github.shadowsocks"><h4>Android</h4></a>
-            <p>Android客户端</p>
-            <h4><a href="http://sourceforge.net/projects/shadowsocksgui/files/dist/">Shadowsocks C#</a></h4>
-            <p> Windows用户推荐此客户端。</p>
-        </div>
-
-        <div class="col-lg-6">
-            <a href="https://itunes.apple.com/us/app/shadowsocks/id665729974?mt=8"><h4>iOS</h4></a>
-            <p>iOS客户端</p>
-
-            <h4><a href="https://github.com/ohdarling/GoAgentX/releases">GoAgentX</a></h4>
-            <p> OS X用户推荐此客户端。</p>
-        </div>
-    </div><?php
->>>>>>> b70c1f1... fix some bad strings for O.C.D
+<?php
             include_once 'footer.php';
             include_once 'ana.php';?>
-</div> <!-- /container -->
+</div>
+<!-- /container -->
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="asset/js/ie10-viewport-bug-workaround.js"></script>
 </body>
